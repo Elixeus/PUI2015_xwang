a0a3bf1a571931da9acb6f7e41403ca7503d87ba
Elixeus <eliseowang@gmail.com>
2015-09-26 13:38:37 -0400

    delete other files

--- task2.py
+++
@@ -1,8 +0,0 @@
-import datetime
-import json
-import sys
-
-if __name__=='__main__':
-    jsonFile = open(sys.argv[1], 'r')
-    metadata = json.load(jsonFile)
-    print datetime.datetime.fromtimestamp(metadata['createdAt'])
